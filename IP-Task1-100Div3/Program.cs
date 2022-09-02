cd9181a2d376e4678b17103e12dfe78d3685b567
Beniamin Kwak <beniaminkwak@gmail.com>
2022-09-02 09:16:01 +0200

    Add project files.

---
+++ IP-Task1-100Div3/Program.cs
@@ -0,0 +1,22 @@
+﻿// 2.	Program, który wypisze wszystkie liczby od 1 do 100, które są podzielne przez 3
+
+namespace IP_Task1_100Div3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+ 
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
